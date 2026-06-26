ccae26a7ab3d81783365a2af1d3d858dcd58a0a1
Peter S <peterpreneur@gmail.com>
2026-06-26 00:25:04 -0500

    post controller

---
+++ src/main/java/com/peterpreneur/accounts/controller/AccountController.java
@@ -0,0 +1,28 @@
+package com.peterpreneur.accounts.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.peterpreneur.accounts.dto.AccountResponse;
+import com.peterpreneur.accounts.dto.CreateAccountRequest;
+import com.peterpreneur.accounts.service.AccountService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/accounts")
+@RequiredArgsConstructor
+public class AccountController {
+
+    private final AccountService accountService;
+
+    @PostMapping
+    public ResponseEntity<AccountResponse> createAccount(@RequestBody CreateAccountRequest request) {
+        AccountResponse response = accountService.createAccount(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);    
+    }
+}
